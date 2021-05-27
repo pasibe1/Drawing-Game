558f606c6ce39bce91d3e506669e537c8d5e1571
Pascal Ibe <pasibe1628634@gmail.com>
2021-05-27 16:13:29 -0500

    Adding entire codebase

---
+++ javascript.js
@@ -0,0 +1,89 @@
+$(function() {
+  var paint = false;
+  var paint_erase = "paint";
+  var canvas = document.getElementById("paint");
+  var ctx = canvas.getContext("2d");
+  var container = $("#container");
+  var mouse = { x: 0, y: 0 };
+
+  if (localStorage.getItem("imgCanvas") != null) {
+    var img = new Image();
+    img.onload = function() {
+      ctx.drawImage(img, 0, 0);
+    };
+    img.src = localStorage.getItem("imgCanvas");
+  }
+
+  ctx.lineWidth = 3;
+  ctx.lineJoin = "round";
+  ctx.lineCap = "round";
+
+  container.mousedown(function(event) {
+    paint = true;
+    ctx.beginPath();
+    mouse.x = event.pageX - this.offsetLeft;
+    mouse.y = event.pageY - this.offsetTop;
+    ctx.moveTo(mouse.x, mouse.y);
+  });
+
+  container.mousemove(function(event) {
+    mouse.x = event.pageX - this.offsetLeft;
+    mouse.y = event.pageY - this.offsetTop;
+    if (paint == true) {
+      if (paint_erase == "paint") {
+        //get color input
+        ctx.strokeStyle = $("#paintColor").val();
+      } else {
+        //white color
+        ctx.strokeStyle = "white";
+      }
+      ctx.lineTo(mouse.x, mouse.y);
+      ctx.stroke();
+    }
+  });
+
+  container.mouseup(function() {
+    paint = false;
+  });
+
+  container.mouseleave(function() {
+    paint = false;
+  });
+
+  $("#erase").click(function() {
+    if (paint_erase == "paint") {
+      paint_erase = "erase";
+    } else {
+      paint_erase = "paint";
+    }
+    $(this).toggleClass("eraseMode");
+  });
+
+  $("#slider").slider({
+    min: 3,
+    max: 30,
+    slide: function(event, ui) {
+      $("#circle").height(ui.value);
+      $("#circle").width(ui.value);
+      ctx.lineWidth = ui.value;
+    }
+  });
+
+  $("#paintColor").change(function() {
+    $("#circle").css("background-color", $(this).val());
+  });
+
+  $("#reset").click(function() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    paint_erase = "paint";
+    $("#erase").removeClass("eraseMode");
+  });
+
+  $("#save").click(function() {
+    if (typeof localStorage != null) {
+      localStorage.setItem("imgCanvas", canvas.toDataURL());
+    } else {
+      window.alert("Your browser does not support local storage!");
+    }
+  });
+});
